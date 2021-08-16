6d60916a142c613b8f5c2e20693a36786ad8fc4a
tnadar <tnadar@LIN25005237.corp.capgemini.com>
2021-08-16 13:55:53 +0530

    First Commit

---
+++ firstdemo/src/main/java/firstdemo/MainClass.java
@@ -0,0 +1,11 @@
+package firstdemo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("Good Afternoon");
+		System.out.println("Welcome to Git!");
+	}
+
+}
